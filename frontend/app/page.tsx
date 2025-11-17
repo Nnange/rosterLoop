e7cce14de0f96a003f7df2c219d1cd5ebfe62fc3
Nnange <awong.nnange@yahoo.com>
2025-11-17 15:50:37 +0100

    feat: implement setup and reset functionality for roommate management

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,46 @@
+"use client"
+
 import Image from "next/image";
 import Header from "./components/Header";
 import { SetupForm } from "./components/SetupForm";
+import { useState } from "react";
+import { CleaningSchedule } from "./components/CleaningSchedule";
 
 export default function Home() {
+  const [isSetup, setIsSetup] = useState(false);
+  const [roommates, setRoommates] = useState<string[]>([]);
+  
+  function handleSetupComplete(names: string[]): void {
+    setRoommates(names);
+    setIsSetup(true);
+    localStorage.setItem('roommates', JSON.stringify(names))
+  }
+
+  function handleReset(): void {
+    localStorage.removeItem('roommates');
+    setRoommates([]);
+    setIsSetup(false);
+  }
+
   return (
     <div className="flex flex-col min-h-screen items-center justify-center ">
       <Header /> 
-      <SetupForm  />
+      {!isSetup ? (
+        <SetupForm onComplete={ handleSetupComplete } />
+      ) : (
+        <>
+            <CleaningSchedule roommates={roommates} />
+            <div className="mt-8 text-center">
+              <button
+                onClick={handleReset}
+                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition-colors"
+              >
+                Reset Roster
+              </button>
+            </div>
+          </>
+      )
+    }
     </div>
   );
 }
